51ff66fd2d8488c587caf80d1c2f751af1bded78
afsheenkhan-24 <afsheenkhan11@yahoo.com>
2025-06-09 21:13:55 +0100

    ch2 complete hello world and variable

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,17 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+# print() -> inbuilt func to print to console
+print("Hello World!")
 
+# input -> inbuilt func to take input from user
+name = input("What is your name?")
+print("Nice to meet you", name)
+
+# defining func, can be reused
+# indentation is important
+def main():
+    print("Start learning Afsheen!")
+
+if __name__ == "__main__":
+    main()
--- Ch2 - Basics/variables_start.py
+++ Ch2 - Basics/variables_start.py
@@ -10,6 +10,7 @@ myfloat = 13.2
 mystr = "This is a string"
 mybool = True
 mylist = [0, 1, "two", 3.2, False]
+# tuple is immutable means can't be changed
 mytuple = (0, 1, 2)
 mydict = {"one" : 1, "two" : 2}
 
@@ -22,16 +23,32 @@ print(mytuple)
 print(mydict)
 
 # re-declaring a variable works
+myint = "abc"
+print(myint)
 
 # to access a member of a sequence type, use []
+print(mylist[2])
+print(mytuple[1])
 
 # use slices to get parts of a sequence
+print(mylist[1:3])
+print(mylist[1:5:2])
 
 # you can use slices to reverse a sequence
+print(mylist[::-1])
+# default start and end with -1 for reverse
 
 # dictionaries are accessed via keys
+print(mydict["one"])
+print(mydict["two"])
 
 # ERROR: variables of different types cannot be combined
+print("my string" + str(12))
 
 # Global vs. local variables in functions
+def main():
+    mystr = "test local"
+    print(mystr)
 
+main()
+print(mystr)
